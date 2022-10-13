a2d099c92db60bda9a93b14e52c22ad659aaca8b
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2022-10-13 09:58:36 -0400

    update aerial setter weights

--- setter.py
+++ setter.py
@@ -61,7 +61,7 @@ class CoyoteSetter(DynamicGMSetter):
                             AugmentSetter(WallDribble(), True, False, False),
                             AugmentSetter(RandomState(cars_on_ground=False)),
                         ),
-                        (0.25, 0.25, 0.13, 0.20, 0.15, 0.02)
+                        (0.05, 0.30, 0.20, 0.29, 0.15, 0.01)
                     )
                 )
         elif mode == "pinch":
