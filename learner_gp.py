6893143c66f9063ff786b048521738f1ca19ff1b
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2022-11-05 12:27:04 -0400

    unfreeze appx 1b

--- learner_gp.py
+++ learner_gp.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     fps = 120 / frame_skip
     gamma = np.exp(np.log(0.5) / (fps * half_life_seconds))
     config = dict(
-        actor_lr=0,  # 1e-4,
+        actor_lr=1e-4,
         critic_lr=1e-4,
         n_steps=Constants_gp.STEP_SIZE,
         batch_size=100_000,
@@ -146,7 +146,7 @@ if __name__ == "__main__":
         disable_gradient_logging=True,
     )
 
-    alg.load("GP_saves/Opti_1667532196.7084641/Opti_4120/checkpoint.pt")
+    alg.load("GP_saves/Opti_1667618173.6571293/Opti_4320/checkpoint.pt")
     alg.agent.optimizer.param_groups[0]["lr"] = logger.config.actor_lr
     alg.agent.optimizer.param_groups[1]["lr"] = logger.config.critic_lr
 
