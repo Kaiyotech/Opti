0ff5b223c485009ff054f2a00c5cae995795e4dd
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2022-11-17 10:13:22 -0500

    flip reset reset

--- CoyoteParser.py
+++ CoyoteParser.py
@@ -9,37 +9,63 @@ from CoyoteObs import CoyoteObsBuilder
 
 
 class CoyoteAction(ActionParser):
-    def __init__(self):
+    def __init__(self, version=None):
         super().__init__()
-        self._lookup_table = self.make_lookup_table()
+        self._lookup_table = self.make_lookup_table(version)
 
     @staticmethod
-    def make_lookup_table():
+    def make_lookup_table(version):
         actions = []
-        # Ground
-        for throttle in (-1, 0, 0.5, 1):
-            for steer in (-1, -0.5, 0, 0.5, 1):
-                for boost in (0, 1):
-                    for handbrake in (0, 1):
+        if version is None or version == "Normal":
+            # Ground
+            for throttle in (-1, 0, 0.5, 1):
+                for steer in (-1, -0.5, 0, 0.5, 1):
+                    for boost in (0, 1):
+                        for handbrake in (0, 1):
+                            if boost == 1 and throttle != 1:
+                                continue
+                            actions.append([throttle or boost, steer, 0, steer, 0, 0, boost, handbrake])
+            # Aerial
+            for pitch in (-1, -0.75, -0.5, 0, 0.5, 0.75, 1):
+                for yaw in (-1, -0.75, -0.5, 0, 0.5, 0.75, 1):
+                    for roll in (-1, 0, 1):
+                        for jump in (0, 1):
+                            for boost in (0, 1):
+                                if jump == 1 and yaw != 0:  # Only need roll for sideflip
+                                    continue
+                                if pitch == roll == jump == 0:  # Duplicate with ground
+                                    continue
+                                # Enable handbrake for potential wavedashes
+                                handbrake = jump == 1 and (pitch != 0 or yaw != 0 or roll != 0)
+                                actions.append([boost, yaw, pitch, yaw, roll, jump, boost, handbrake])
+            # append stall
+            actions.append([0, 1, 0, 0, -1, 1, 0, 0])
+            actions = np.array(actions)
+
+        elif version == "flip_reset":
+            # Ground
+            for throttle in (-1, 0, 1):
+                for steer in (-1, 0, 1):
+                    for boost in (0, 1):
                         if boost == 1 and throttle != 1:
                             continue
-                        actions.append([throttle or boost, steer, 0, steer, 0, 0, boost, handbrake])
-        # Aerial
-        for pitch in (-1, -0.75, -0.5, 0, 0.5, 0.75, 1):
-            for yaw in (-1, -0.75, -0.5, 0, 0.5, 0.75, 1):
-                for roll in (-1, 0, 1):
-                    for jump in (0, 1):
-                        for boost in (0, 1):
-                            if jump == 1 and yaw != 0:  # Only need roll for sideflip
-                                continue
-                            if pitch == roll == jump == 0:  # Duplicate with ground
-                                continue
-                            # Enable handbrake for potential wavedashes
-                            handbrake = jump == 1 and (pitch != 0 or yaw != 0 or roll != 0)
-                            actions.append([boost, yaw, pitch, yaw, roll, jump, boost, handbrake])
-        # append stall
-        actions.append([0, 1, 0, 0, -1, 1, 0, 0])
-        actions = np.array(actions)
+                        actions.append([throttle or boost, steer, 0, steer, 0, 0, boost, 0])
+            # Aerial
+            for pitch in (-1, 0, 1):
+                for yaw in (-1, 0, 1):
+                    for roll in (-1, 0, 1):
+                        for jump in (0, 1):
+                            for boost in (0, 1):
+                                if jump == 1 and yaw != 0 or roll != 0:  # no flips necessary here
+                                    continue
+                                if pitch == roll == jump == 0:  # Duplicate with ground
+                                    continue
+                                # Enable handbrake for potential wavedashes
+                                actions.append([boost, yaw, pitch, yaw, roll, jump, boost, 0])
+            # append stall
+            actions.append([0, 1, 0, 0, -1, 1, 0, 0])
+            actions = np.array(actions)
+
         return actions
 
     def get_action_space(self) -> gym.spaces.Space:
--- learner_flip_reset.py
+++ learner_flip_reset.py
@@ -51,10 +51,10 @@ if __name__ == "__main__":
         ent_coef=0.01,
     )
 
-    run_id = "flip_reset_run1"
+    run_id = "flip_reset_run2"
     wandb.login(key=os.environ["WANDB_KEY"])
     logger = wandb.init(dir="./wandb_store",
-                        name="Flip_reset_1",
+                        name="Flip_reset_2",
                         project="Opti",
                         entity="kaiyotech",
                         id=run_id,
@@ -65,11 +65,11 @@ if __name__ == "__main__":
                   db=Constants_flip_reset.DB_NUM)  # host="192.168.0.201",
     redis.delete("worker-ids")
 
-    stat_trackers = [
-        Speed(normalize=True), Demos(), TimeoutRate(), Touch(), EpisodeLength(), Boost(), BehindBall(), TouchHeight(),
-        DistToBall(), AirTouch(), AirTouchHeight(), BallHeight(), BallSpeed(normalize=True), CarOnGround(),
-        GoalSpeed(), MaxGoalSpeed(),
-    ]
+    # stat_trackers = [
+    #     Speed(normalize=True), Demos(), TimeoutRate(), Touch(), EpisodeLength(), Boost(), BehindBall(), TouchHeight(),
+    #     DistToBall(), AirTouch(), AirTouchHeight(), BallHeight(), BallSpeed(normalize=True), CarOnGround(),
+    #     GoalSpeed(), MaxGoalSpeed(),
+    # ]
 
     rollout_gen = RedisRolloutGenerator("Flip_reset",
                                         redis,
@@ -77,40 +77,32 @@ if __name__ == "__main__":
                                                                  tick_skip=Constants_flip_reset.FRAME_SKIP,
                                                                  team_size=3, extra_boost_info=False),
                                         lambda: ZeroSumReward(zero_sum=Constants_flip_reset.ZERO_SUM,
-                                                              goal_w=2,
-                                                              aerial_goal_w=0,
-                                                              double_tap_w=0,
                                                               flip_reset_w=10,
-                                                              flip_reset_goal_w=20,
                                                               punish_ceiling_pinch_w=-5,
-                                                              concede_w=-10,
-                                                              velocity_bg_w=0.25,
-                                                              velocity_pb_w=0.05,
-                                                              acel_ball_w=0,
-                                                              team_spirit=0,
-                                                              cons_air_touches_w=0.02,
+                                                              velocity_bg_w=0.4,
+                                                              velocity_pb_w=0.25,
                                                               jump_touch_w=0.25,
                                                               wall_touch_w=0.5,
                                                               ),
-                                        lambda: CoyoteAction(),
+                                        lambda: CoyoteAction("flip_reset"),
                                         save_every=logger.config.save_every * 3,
                                         model_every=logger.config.model_every,
                                         logger=logger,
                                         clear=False,
-                                        stat_trackers=stat_trackers,
+                                        # stat_trackers=stat_trackers,
                                         # gamemodes=("1v1", "2v2", "3v3"),
                                         max_age=1,
                                         )
 
     critic = Sequential(Linear(222, 256), LeakyReLU(), Linear(256, 256), LeakyReLU(),
-                        Linear(256, 256), LeakyReLU(), Linear(256, 256), LeakyReLU(),
+                        Linear(256, 256), LeakyReLU(),
                         Linear(256, 1))
 
-    actor = Sequential(Linear(222, 256), LeakyReLU(), Linear(256, 256), LeakyReLU(),
-                       Linear(256, 256), LeakyReLU(),
-                       Linear(256, 373))
+    actor = Sequential(Linear(222, 256), LeakyReLU(), Linear(256, 128), LeakyReLU(),
+                       Linear(128, 128), LeakyReLU(),
+                       Linear(128, 31))
 
-    actor = DiscretePolicy(actor, (373,))
+    actor = DiscretePolicy(actor, (31,))
 
     optim = torch.optim.Adam([
         {"params": actor.parameters(), "lr": logger.config.actor_lr},
@@ -135,7 +127,7 @@ if __name__ == "__main__":
         disable_gradient_logging=True,
     )
 
-    alg.load("flip_reset_saves/Opti_1666625461.6042824/Opti_530/checkpoint.pt")
+    # alg.load("flip_reset_saves/Opti_1666625461.6042824/Opti_530/checkpoint.pt")
     alg.agent.optimizer.param_groups[0]["lr"] = logger.config.actor_lr
     alg.agent.optimizer.param_groups[1]["lr"] = logger.config.critic_lr
 
