961b7b401129f45242946e89596eaeacf7ae9d79
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2022-11-21 12:10:50 -0500

    get ready to change gamestate for gp weights. Hopefully I remember to delete the key from redis...

--- worker_gp.py
+++ worker_gp.py
@@ -116,7 +116,7 @@ if __name__ == "__main__":
                                 send_obs=True,
                                 auto_minimize=auto_minimize,
                                 send_gamestates=send_gamestate,
-                                gamemode_weights={'1v1': 0.40, '2v2': 0.20, '3v3': 0.40},  # default 1/3
+                                gamemode_weights={'1v1': 0.20, '2v2': 0.35, '3v3': 0.45},  # default 1/3
                                 streamer_mode=streamer_mode,
                                 deterministic_streamer=deterministic_streamer,
                                 force_old_deterministic=force_old_deterministic,
