de1ce8ec209071539ab2b5c990a017e781c2f398
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2023-01-30 12:10:51 -0500

    testing dodge

--- CoyoteParser.py
+++ CoyoteParser.py
@@ -75,6 +75,35 @@ class CoyoteAction(ActionParser):
             # actions.append([0, 1, 0, 0, -1, 1, 0, 0])
             actions = np.array(actions)
 
+        elif version == "test_dodge":
+            # Ground
+            for throttle in (-1, 0, 0.5, 1):
+                for steer in (-1, -0.5, 0, 0.5, 1):
+                    for boost in (0, 1):
+                        for handbrake in (0, 1):
+                            if boost == 1 and throttle != 1:
+                                continue
+                            actions.append(
+                                [throttle or boost, steer, 0, steer, 0, 0, boost, handbrake])
+            # Aerial
+            for pitch in (-0.85, -0.84, -0.83, 0, 0.83, 0.84, 0.85):
+                for yaw in (0, 0, 0, 0, 0, 0, 0):
+                    for roll in (0, 0, 0):
+                        for jump in (0, 1):
+                            for boost in (0, 1):
+                                if jump == 1 and yaw != 0:  # Only need roll for sideflip
+                                    continue
+                                if pitch == roll == jump == 0:  # Duplicate with ground
+                                    continue
+                                # Enable handbrake for potential wavedashes
+                                handbrake = jump == 1 and (
+                                        pitch != 0 or yaw != 0 or roll != 0)
+                                actions.append(
+                                    [boost, yaw, pitch, yaw, roll, jump, boost, handbrake])
+            # append stall
+            actions.append([0, 1, 0, 0, -1, 1, 0, 0])
+            actions = np.array(actions)
+
         return actions
 
     def get_action_space(self) -> gym.spaces.Space:
