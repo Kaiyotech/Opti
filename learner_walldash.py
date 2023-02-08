70ce935f3dbfe98b047a4dd7ae27123c50424661
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2023-02-08 07:12:17 -0500

    restart from scratch walldash. Fix object, slight reward weight.

--- learner_walldash.py
+++ learner_walldash.py
@@ -56,10 +56,10 @@ if __name__ == "__main__":
         ent_coef=0.01,
     )
 
-    run_id = "walldash_run2.00"
+    run_id = "walldash_run3.00"
     wandb.login(key=os.environ["WANDB_KEY"])
     logger = wandb.init(dir="./wandb_store",
-                        name="Walldash_Run2.00",
+                        name="Walldash_Run3.00",
                         project="Opti",
                         entity="kaiyotech",
                         id=run_id,
