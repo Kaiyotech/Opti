29aec93d7aa3521e4dff4f3a6d349fec73204be2
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2023-02-22 12:00:15 -0500

    putting eval prob back from testing

--- worker_selector.py
+++ worker_selector.py
@@ -287,7 +287,7 @@ if __name__ == "__main__":
     local = True
     auto_minimize = True
     game_speed = 100
-    evaluation_prob = 0.5  # 0.01
+    evaluation_prob = 0.01
     past_version_prob = 0.1
     deterministic_streamer = False
     force_old_deterministic = False
