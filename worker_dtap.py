0715e10be605ace863c15b1e4843fd35be1e605f
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2023-05-11 13:48:27 -0400

    fix regular rocket league obs init on dtap

--- worker_dtap.py
+++ worker_dtap.py
@@ -110,6 +110,7 @@ if __name__ == "__main__":
                                      add_fliptime=True,
                                      add_boosttime=True,
                                      add_handbrake=True,
+                                     doubletap_indicator=True,
                                      ),
         action_parser=CoyoteAction(),
         terminal_conditions=[GoalScoredCondition(),
