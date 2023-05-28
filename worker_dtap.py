b03cc9944e8ff928a1f16c66721d033e2fbd6a22
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2023-05-27 23:42:42 -0400

    dtap change 1v0 weight.

--- worker_dtap.py
+++ worker_dtap.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
     non_latest_version_prob = [1, 0, 0, 0]  # [0.825, 0.0826, 0.0578, 0.0346]  # this includes past_version and pretrained
     deterministic_streamer = True
     force_old_deterministic = False
-    gamemode_weights = {'1v0': 0.4, '1v1': 0.6}
+    gamemode_weights = {'1v0': 0.75, '1v1': 0.25}
     visualize = False
     simulator = True
     batch_mode = True
