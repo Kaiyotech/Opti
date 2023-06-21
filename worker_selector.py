203868ad79b47c47ce693fd8391233d6834e52a6
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2023-06-21 14:24:11 -0400

    streamer things

--- worker_selector.py
+++ worker_selector.py
@@ -373,7 +373,7 @@ if __name__ == "__main__":
     game_speed = 100
     evaluation_prob = 0.01
     past_version_prob = 0
-    deterministic_streamer = False
+    deterministic_streamer = True
     force_old_deterministic = False
     team_size = 3
     dynamic_game = True
