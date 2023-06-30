34313166f316ad4084ecb92bfc1d6468ab2dfca5
Kaiyotech <93724202+Kaiyotech@users.noreply.github.com>
2023-06-30 09:46:53 -0400

    starting 23, fixed selector skip schedule, increased wall rewards, added ball height reward/punish.

--- learner_selector.py
+++ learner_selector.py
@@ -57,10 +57,10 @@ if __name__ == "__main__":
         ent_coef=0.01,
     )
 
-    run_id = "selector_run_22.01"
+    run_id = "selector_run_23.00"
     wandb.login(key=os.environ["WANDB_KEY"])
     logger = wandb.init(dir="./wandb_store",
-                        name="Selector_Run_22.01",
+                        name="Selector_Run_23.00",
                         project="Opti",
                         entity="kaiyotech",
                         id=run_id,
@@ -221,7 +221,7 @@ if __name__ == "__main__":
         max_grad_norm=None,
     )
 
-    alg.load("Selector_saves/Opti_1688062788.9954789/Opti_5/checkpoint.pt")
+    # alg.load("Selector_saves/Opti_1688062788.9954789/Opti_5/checkpoint.pt")
 
     alg.agent.optimizer.param_groups[0]["lr"] = logger.config.actor_lr
     alg.agent.optimizer.param_groups[1]["lr"] = logger.config.critic_lr
